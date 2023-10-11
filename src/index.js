746d26ccf549710fae271ec7d33bb2b108de80d8
oleg233334 <142994486+oleg233334@users.noreply.github.com>
2023-10-11 23:58:24 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function filterPrimeNumbers(arr) {
+    function isPrime(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    return arr.filter((number) => isPrime(number));
+}
+
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const primeNumbersArray = filterPrimeNumbers(numbersArray);
+console.log(primeNumbersArray);
